319d6f3105469862ddac238dea0e8bcc7c7706f9
arthur_avezhentsev <celt244@gmail.com>
2016-11-18 21:04:42 +0200

    mod lesson5

--- Lesson5/src/Main.java
+++ Lesson5/src/Main.java
@@ -1,30 +1,7 @@
 import java.util.Arrays;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        for (int i = 10; i >= 0; i--) {
-            int sqrt = i * i;
-            int cqrt = sqrt * i;
-
-            System.out.println("Квадрат числа " + i + " = " + (sqrt));
-            System.out.println("Куб числа " + i + " = " + (cqrt));
-
-
-        }
-
-
-    int [] arr2 = {2,4,5,6,7,};
-        modeArr(arr2);
-        System.out.println(Arrays.toString(arr2));
-
-
-}
-
-
-
-    public static int[] modeArr(int[] arr) {
+    public static int[] modeArr (int[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
 
@@ -32,3 +9,23 @@ public class Main {
         }
         return arr;
     }
+    public static void main(String[] args) {
+
+//        for (int i = 10; i >= 0; i--) {
+//            int sqrt = i * i;
+//            int cqrt = sqrt * i;
+//
+//            System.out.println("Квадрат числа " + i + " = " + (sqrt));
+//            System.out.println("Куб числа " + i + " = " + (cqrt));
+//
+//
+//        }
+
+        int[] arr2 = {2, 4, 5, 6, 7,};
+        modeArr(arr2);
+        System.out.println(Arrays.toString(arr2));
+    }
+}
+
+
+
