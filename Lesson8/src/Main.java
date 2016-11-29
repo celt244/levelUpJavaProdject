36184f7c025d036cb615da0b4f87f853369d4703
arthur_avezhentsev <celt244@gmail.com>
2016-11-29 20:58:37 +0200

    add lesson8

---
+++ Lesson8/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 90;
+        while (a >= 0){
+            System.out.print(a + " ");
+            a -=5;
+        }
+        System.out.println();
+//////////////////////////////////////////
+        int b = 257;
+        do {
+            b /= 10;
+            System.out.println(b);
+
+        } while (b < 0);
+/////////////////////////////////////////////////////////////////////
+        String [] name = {"Ethan Hawke", "Julie Deply"};
+
+        for(String s : name){
+            System.out.println("Name: " + s);}
+///////////////////////////////////////////////////////////////////////
+        String[] str = {"this", "is", "a", "very", "long", "array", "which", "has", "absolutely", "no", "sense"};
+
+        for (String s1 : str){
+
+            System.out.println(s1 + " - " + s1.length());
+        }
+
+
+
+
+
+    }
+}
