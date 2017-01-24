56a06b0727907e37f9104424ea5a163c07a0d983
arthur.svezhentsev <java@training.workrocks.ua>
2017-01-24 20:46:00 +0200

    lesson 20

---
+++ Lesson20/src/ua/dp/levelup/list/AbstractList.java
@@ -0,0 +1,32 @@
+package ua.dp.levelup.list;
+
+import java.util.Optional;
+
+public abstract class AbstractList {
+
+    protected int size = 0;
+
+    public int size(){
+        return size;
+    }
+
+    public abstract void addFirst(Node node);
+
+    public abstract void addLast(Node node);
+
+    public abstract void removeFirst();
+
+    public abstract void removeLast();
+
+    public abstract Optional<Node> getFirst();
+
+    public abstract Optional<Node> getLast();
+
+    public abstract Optional<Node> get(int index);
+
+    public abstract boolean isEmpty();
+
+    public abstract boolean isNotEmpty();
+
+
+}
---
+++ Lesson20/src/ua/dp/levelup/list/SingleLinkedListTest.groovy
@@ -0,0 +1,62 @@
+package ua.dp.levelup.list
+
+import spock.lang.Specification
+
+/**
+ * Created by java on 24.01.2017.
+ */
+class SingleLinkedListTest extends Specification {
+    def "add first node to empty list"() {
+        SingleLinkedList list = new SingleLinkedList();
+
+        when: 'add node element'
+        list.addFirst(new Node (1));
+
+        then:'check size'
+        list.size() == 1;
+        list.getFirst().isPresent()
+        list.getFirst().get().value == 1
+
+    }
+
+    def "add first node to list with one element"() {
+        SingleLinkedList list = new SingleLinkedList();
+
+        list.addFirst(new Node (1));
+
+        when: 'add one more node element'
+        list.addFirst(new Node (5));
+
+        then:'check size'
+        list.size == 2;
+        list.getFirst().isPresent()
+        list.getFirst().get().value == 5
+    }
+
+    def "add null node to empty list"() {
+        SingleLinkedList list = new SingleLinkedList();
+
+        when: 'add node element'
+        list.addFirst(null);
+
+        then:'check size'
+        list.size() == 0;
+        !list.getFirst().isPresent()
+    }
+
+    def "addFirst null to list with one element"() {
+        SingleLinkedList list = new SingleLinkedList();
+
+        list.addFirst(new Node (1));
+
+        when: 'add one more node element'
+        list.addFirst(new Node (5));
+
+        then:'check size'
+        list.size == 2;
+        list.getFirst().isPresent()
+        list.getFirst().get().value == 5
+    }
+
+
+}
