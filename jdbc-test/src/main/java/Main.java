c55511b39d3bf697db4076ba691557fa3766013f
ArthurSvezhentsev <celt244@gmail.com>
2017-03-23 23:48:30 +0200

    wokk in gdbc

--- jdbc-test/src/main/java/Main.java
+++ jdbc-test/src/main/java/Main.java
@@ -1,8 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by java on 17.03.2017.
@@ -10,20 +8,45 @@ import java.sql.Statement;
 
 public class Main {
 
-  public static void main(String[] args) {
-    try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
-      Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/forum", "root", "qwerty");
-      Statement statement = connection.createStatement();
-      ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+  public static void main(String[] args) throws ClassNotFoundException, SQLException, IllegalAccessException, InstantiationException {
+    Class.forName("com.mysql.jdbc.Driver");
 
-      while (resultSet.next()) {
-        System.out.println(resultSet.getLong("ID") + " " + resultSet.getString("USERNAME") + " " + resultSet.getString("EMAIL"));
-      }
+    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/forum",
+            "root", "qwerty");
 
-      connection.close();
-    } catch (SQLException | ClassNotFoundException e) {
-      e.printStackTrace();
+    PreparedStatement preparedStatement = connection.prepareStatement
+            ("INSERT INTO USERS (USERSNAME, EMAIL, PASSWORD) VALUES(?,?,?)");
+
+    User user = new User("Bob Martin", "bobm@yopmail.com", "1234");
+
+    preparedStatement.setString(1, user.getName());
+    preparedStatement.setString(2, user.getEmail());
+    preparedStatement.setString(3, user.getPassword());
+
+    preparedStatement.execute();
+
+
+    Statement statement = connection.createStatement();
+
+    ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+
+    List<User> userList = new ArrayList<User>();
+    while (resultSet.next()) {
+      long id = resultSet.getLong("ID");
+      String usersname = resultSet.getString("USERSNAME");
+      String email = resultSet.getString("EMAIL");
+      String pass = resultSet.getString("PASSWORD");
+
+      userList.add(new User(id, usersname, email, pass));
+
+      System.out.println(resultSet.getLong("ID")
+              + " " + resultSet.getString("USERNAME")
+              + " " + resultSet.getString("EMAIL"));
     }
+
+
+    System.out.println(userList);
+
+    connection.close();
   }
 }
