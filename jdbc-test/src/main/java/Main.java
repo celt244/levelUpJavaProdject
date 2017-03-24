4d9403a723bf6bf9f221b4ac5e121acca84267c0
ZZ0000 <jon555@ua.fm>
2017-03-24 20:51:17 +0200

    jdbc

--- jdbc-test/src/main/java/Main.java
+++ jdbc-test/src/main/java/Main.java
@@ -11,13 +11,13 @@ public class Main {
   public static void main(String[] args) throws ClassNotFoundException, SQLException, IllegalAccessException, InstantiationException {
     Class.forName("com.mysql.jdbc.Driver");
 
-    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/forum",
-            "root", "qwerty");
+    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/forum", "root", "qwerty");
 
     PreparedStatement preparedStatement = connection.prepareStatement
-            ("INSERT INTO USERS (USERSNAME, EMAIL, PASSWORD) VALUES(?,?,?)");
+            ("INSERT INTO USERS (USERSNAME, EMAIL, PASSWORD) VALUES(?,?,?)",
+            Statement.RETURN_GENERATED_KEYS);
 
-    User user = new User("Bob Martin", "bobm@yopmail.com", "1234");
+    User user = new User("Harry Potter", "potter@yopmail.com", "1234");
 
     preparedStatement.setString(1, user.getName());
     preparedStatement.setString(2, user.getEmail());
@@ -25,28 +25,37 @@ public class Main {
 
     preparedStatement.execute();
 
+    ResultSet generatedKeys =  preparedStatement.getGeneratedKeys();
 
-    Statement statement = connection.createStatement();
+    while(generatedKeys.next()){
+      long id = generatedKeys.getLong("GENERATED_KEY");
 
-    ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
-
-    List<User> userList = new ArrayList<User>();
-    while (resultSet.next()) {
-      long id = resultSet.getLong("ID");
-      String usersname = resultSet.getString("USERSNAME");
-      String email = resultSet.getString("EMAIL");
-      String pass = resultSet.getString("PASSWORD");
-
-      userList.add(new User(id, usersname, email, pass));
-
-      System.out.println(resultSet.getLong("ID")
-              + " " + resultSet.getString("USERNAME")
-              + " " + resultSet.getString("EMAIL"));
+      user.setId(id);
     }
 
 
-    System.out.println(userList);
 
+//    Statement statement = connection.createStatement();
+//
+//    ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+//
+//    List<User> userList = new ArrayList<User>();
+//    while (resultSet.next()) {
+//      long id = resultSet.getLong("ID");
+//      String usersname = resultSet.getString("USERSNAME");
+//      String email = resultSet.getString("EMAIL");
+//      String pass = resultSet.getString("PASSWORD");
+//
+//      userList.add(new User(id, usersname, email, pass));
+//
+//      System.out.println(resultSet.getLong("ID")
+//              + " " + resultSet.getString("USERNAME")
+//              + " " + resultSet.getString("EMAIL"));
+//    }
+//
+//
+//    System.out.println(userList);
+//
     connection.close();
   }
 }
